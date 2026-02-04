74ea271b7bebbbe9c6312d56133c3aadbe016b86
Johannest85 <71155346+Johannest85@users.noreply.github.com>
2026-02-04 23:33:14 +0100

    Ändrat lite hur man sätter upp turnering

--- RPGspel/Program.cs
+++ RPGspel/Program.cs
@@ -4,12 +4,13 @@ class Client
 {
     static void Main(string[] args)
     {
-        Character player = new Jedi("Yoda", new Alien());
-        Character player2 = new Jedi("Yoda2", new Elf());
-        Character player3 = new Warrior("Conan", new Orc());
-        Character player4 = new Mage("Gandalf", new Fairy());
-        Character player5 = new Archer("Legolas", new Elf());
-        List<Character> participants = new List<Character> { player, player2, player3, player4, player5 };
+        List<Character> participants = new List<Character> {
+            new Jedi("Yoda", new Alien()),
+            new Jedi("Grogu", new Alien()),
+            new Warrior("Conan", new Orc()),
+            new Mage("Gandalf", new Fairy()),
+            new Archer("Legolas", new Elf())
+        };
         Tournament tournament = new Tournament(participants, 0.5, 80, 0);
         tournament.StartTournament();   
 
